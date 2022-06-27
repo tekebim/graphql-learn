aa794ee823f1204c5a8b41c6159c15bc104aa2bf
Michael R <tekmedias@gmail.com>
2022-06-27 11:18:28 +0200

    :sparkles: Add movies object on resolver

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,5 +1,23 @@
 const hello = "hello world !";
 
+const movies = [
+  {
+    id: 1,
+    title: "Star Wars : L'attaque des clones",
+    plot: "Film avec des sabres lasers."
+  },
+  {
+    id: 2,
+    title: "Joker",
+    plot: "Un clown pas content"
+  },
+  {
+    id: 3,
+    title: "Jurassic World",
+    plot: "Des dinosaures qui s'échappent"
+  }
+]
+
 const resolvers = {
   Query: {
     // Query fait référence au type Query du schema.graphql
